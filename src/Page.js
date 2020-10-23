de68518e81cfa49aac5a7bcbcf4f2d654119fbe1
Jim Stockwell <jimstockwellatwork@yahoo.com>
2020-10-22 20:21:51 -0700

    add Edit, Cancel, Save buttons

--- src/Page.js
+++ src/Page.js
@@ -3,24 +3,26 @@ import { TableConfigurable } from "./Table"
 
 const Page = (props) => {
 
-    const [portfolio,setPortfolio] = useState(null);
-    useEffect( () => {
+    const [portfolio, setPortfolio] = useState(null);
+    useEffect(() => {
         let mounted = true;
         const fetchPortfolio = async () => {
-            if(props.backend) {
-                props.backend.getPortfolio().then( data => {
-                    if(mounted) {
+            if (props.backend) {
+                props.backend.getPortfolio().then(data => {
+                    if (mounted) {
                         setPortfolio(data)
                     }
-                }).catch( error => {
-                        console.log("Error from props.backend.getPortfolio():");
-                        console.log(error);
+                }).catch(error => {
+                    console.log("Error from props.backend.getPortfolio():");
+                    console.log(error);
                 });
             }
         }
         fetchPortfolio();
-        return () => {mounted = false};
-    },[props.backend]);
+        return () => { mounted = false };
+    }, [props.backend]);
+
+    const [pageState, setPageState] = useState("regular"); // Alternative is "editing"
 
     const columns = [
         {
@@ -36,12 +38,34 @@ const Page = (props) => {
             accessor: 'pct'
         }
     ];
-    if (portfolio===null) return "Loading...";
+    if (portfolio === null) return "Loading...";
     return (
         <div>
             <TableConfigurable data={portfolio} columns={columns}></TableConfigurable>
+            <PageStateButtons state={pageState} onNewState={setPageState} />
         </div>
     )
+}
+
+/* Attributes:
+ * state = "editing" | "regular"
+ * onNewState = callback, passing new state
+ */
+const PageStateButtons = props => {
+    if (props.state === "regular") {
+        return (
+            <button onClick={() => props.onNewState("editing")}>Edit</button>
+        )
+    }
+    if (props.state == "editing") {
+        return (
+            <div>
+                <button>Cancel</button>
+                <button>Save</button>
+            </div>
+        )
+    }
+    return null;
 }
 
 export { Page };
--- src/Page.test.js
+++ src/Page.test.js
@@ -1,12 +1,12 @@
 "use strict";
 
 import React from 'react';
-import { render, screen, waitForElement } from '@testing-library/react';
+import { render, screen, waitForElement, waitForElementToBeRemoved, fireEvent } from '@testing-library/react';
 import { Page } from './Page';
 import BackendApi from './BackendApi';
 
 test('Page with no data source shows loading message', () => {
-    render( <Page/> );
+    render(<Page />);
 
     const el = screen.getByText(/Loading/);
     expect(el).toBeInTheDocument();
@@ -14,16 +14,47 @@ test('Page with no data source shows loading message', () => {
 
 test('Page can read and display a one line portfolio', async () => {
     const backendApi = BackendApi.createNull();
-    backendApi.setPortfolio([{ticker: "SPX", qty: 700, pct: 100}]);
-    render( <Page backend={backendApi}/>);
+    await backendApi.setPortfolio([{ ticker: "SPX", qty: 700, pct: 100 }]);
+
+    render(<Page backend={backendApi} />);
 
     const el = await waitForElement(() => screen.queryByText(/SPX/));
     expect(el).toBeInTheDocument();
 });
+
+test('When simply displaying the portfolio, there is an edit button (after the portfolio is loaded)', async () => {
+    const backendApi = BackendApi.createNull();
+    await backendApi.setPortfolio([{ ticker: "SPX", qty: 700, pct: 100 }]);
+
+    render(<Page backend={backendApi} />);
+
+    const text = await waitForElement(() => screen.queryByText("Edit"));
+    expect(text).toBeInTheDocument();
+    const button = text.closest("button");
+    expect(button).toBeInTheDocument();
+    const save = screen.queryByText("Save");
+    expect(save).not.toBeInTheDocument();
+    const cancel = screen.queryByText("Cancel");
+    expect(cancel).not.toBeInTheDocument();
+
+})
+
+test('When edit button is clicked, replace with cancel and save choices', async () => {
+    const backendApi = BackendApi.createNull();
+    await backendApi.setPortfolio([{ ticker: "SPX", qty: 700, pct: 100 }]);
+    render(<Page backend={backendApi} />);
+    const text = await waitForElement(() => screen.queryByText("Edit"));
+    const button = text.closest("button");
+
+    fireEvent.click(button);
+
+    expect(screen.queryByText("Edit")).not.toBeInTheDocument();
+    expect(screen.getByText("Cancel")).toBeInTheDocument();
+    expect(screen.getByText("Save")).toBeInTheDocument();
+})
 
 // TODO: Error handling, control, get and merge prices
 //       Not just hard coded, but reflects what is provided by backend.
 //       Confirm that in display state, is not editable
 //       Confirm that in edit state, is infact editable
 //       Confirm that in edit state, edits roll through to calculated fields
-//       Confirm
