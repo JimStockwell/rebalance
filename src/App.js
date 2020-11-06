74700c68a98cabd91cde259347b2d0e16e88a0e7
Jim Stockwell <jimstockwellatwork@yahoo.com>
2020-11-06 09:14:09 -0700

    feat(App): add a sign out button

--- src/App.js
+++ src/App.js
@@ -4,17 +4,30 @@ import BackendApi from './BackendApi';
 import { Page } from './Page';
 
 import { withAuthenticator } from '@aws-amplify/ui-react'
-import { API } from 'aws-amplify';
+import { API, Auth } from 'aws-amplify';
 
 API.configure();
 
 function AppBase() {
   const backendApi = BackendApi.create();
 
+  const signOut = async () => {
+    await Auth.signOut()
+      .then(data => {
+        return this.props.onStateChange('signedOut')
+      })
+      .catch(err => {/*handle error*/});
+    window.location.reload();
+  }
+  
   return (
-    <Page backend={backendApi} />
+    <div>
+      <Page backend={backendApi} />
+      <button onClick={signOut}>Sign Out</button>
+    </div>
   );
 }
+// https://github.com/aws-amplify/amplify-js/issues/1529 - on how to reload the withAuthenticator(AppBase) after log-out
 
 const App = withAuthenticator(AppBase);
 
